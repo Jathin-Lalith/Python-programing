15a8b1289f2a548653cbee629959a22e2ec82eb9
Jathin-Lalith <139066458+Jathin-Lalith@users.noreply.github.com>
2023-08-13 17:18:07 +0530

    Create Code Chef Python code.py

---
+++ Code Chef Python code.py
@@ -0,0 +1,9 @@
+# Update your code below this line
+#Take input for two integer variables a and b
+a=int(input())
+b=int(input())
+#Output "Coding is Fun!" to the console if a is greater than b
+if (a>b):
+    print("Coding is Fun!")
+#If a is less than or equal to b - output nothing
+    
